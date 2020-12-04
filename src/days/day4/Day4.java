57b907b96e03a1d2ffd9197d75c098a422285c36
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2020-12-04 12:00:53 +0100

    Solve Day4

--- src/days/day4/Day4.java
+++ src/days/day4/Day4.java
@@ -1,2 +1,74 @@
-package days.day4;public class Day4 {
+package days.day4;
+
+import riddarvid.aoc.days.Day;
+import riddarvid.aoc.parsing.ParsingUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day4 extends Day {
+    private List<Passport> passports;
+
+    public static void main(String[] args) {
+        new Day4();
+    }
+
+    @Override
+    protected void part1() {
+        int sum = 0;
+        for (Passport passport : passports) {
+            if (passport.allFieldsPresent()) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    @Override
+    protected void part2() {
+        int sum = 0;
+        for (Passport passport : passports) {
+            if (passport.isValid()) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    @Override
+    protected void setup() {
+        passports = new ArrayList<>();
+        Passport current = new Passport();
+        for (String line : lines) {
+            if (line.equals("")) {
+                passports.add(current);
+                current = new Passport();
+            } else {
+                parseLine(current, line);
+            }
+        }
+        passports.add(current);
+    }
+
+    private void parseLine(Passport passport, String line) {
+        List<String> properties = ParsingUtils.getTokens(line, ' ');
+        for (String property : properties) {
+            parseProperty(passport, property);
+        }
+    }
+
+    private void parseProperty(Passport passport, String property) {
+        List<String> tokens = ParsingUtils.getTokens(property, ':');
+        String key = tokens.get(0);
+        switch (key) {
+            case "byr" -> passport.setBirthYear(Integer.parseInt(tokens.get(1)));
+            case "iyr" -> passport.setIssueYear(Integer.parseInt(tokens.get(1)));
+            case "eyr" -> passport.setExpirationYear(Integer.parseInt(tokens.get(1)));
+            case "hgt" -> passport.setHeight(tokens.get(1));
+            case "hcl" -> passport.setHairColor(tokens.get(1));
+            case "ecl" -> passport.setEyeColor(tokens.get(1));
+            case "pid" -> passport.setPassportID(tokens.get(1));
+            case "cid" -> passport.setCountryID(Integer.parseInt(tokens.get(1)));
+        }
+    }
 }
--- src/days/day4/Passport.java
+++ src/days/day4/Passport.java
@@ -1,2 +1,148 @@
-package days.day4;public class Passport {
+package days.day4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Passport {
+    private Integer birthYear;
+    private Integer issueYear;
+    private Integer expirationYear;
+    private String height;
+    private String hairColor;
+    private String eyeColor;
+    private String passportID;
+    private Integer countryID;
+
+    private static Set<String> validColors;
+
+    static {
+        validColors = new HashSet<>();
+        validColors.add("amb");
+        validColors.add("blu");
+        validColors.add("brn");
+        validColors.add("gry");
+        validColors.add("grn");
+        validColors.add("hzl");
+        validColors.add("oth");
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setIssueYear(int issueYear) {
+        this.issueYear = issueYear;
+    }
+
+    public void setExpirationYear(int expirationYear) {
+        this.expirationYear = expirationYear;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setPassportID(String passportID) {
+        this.passportID = passportID;
+    }
+
+    public void setCountryID(int countryID) {
+        this.countryID = countryID;
+    }
+
+    public boolean allFieldsPresent() {
+        return birthYear != null && issueYear != null && expirationYear != null && height != null && hairColor != null
+                && eyeColor != null && passportID != null;
+    }
+
+    private boolean isBirthYearValid() {
+        return birthYear >= 1920 && birthYear <= 2002;
+    }
+
+    private boolean isIssueYearValid() {
+        return issueYear >= 2010 && issueYear <= 2020;
+    }
+
+    private boolean isExpirationYearValid() {
+        return expirationYear >= 2020 && expirationYear <= 2030;
+    }
+
+    private boolean isHeightValid() {
+        if (!(height.endsWith("cm") || height.endsWith("in"))) {
+            return false;
+        }
+        int value = Integer.parseInt(height.substring(0, height.length() - 2));
+        if (height.endsWith("cm")) {
+            return value >= 150 && value <= 193;
+        } else {
+            return value >= 59 && value <= 76;
+        }
+    }
+
+    private boolean isHairColorValid() {
+        if (!hairColor.startsWith("#")) {
+            return false;
+        }
+        if (hairColor.length() != 7) {
+            return false;
+        }
+        for (char c : hairColor.substring(1).toCharArray()) {
+            if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f'))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isEyeColorValid() {
+        return validColors.contains(eyeColor);
+    }
+
+    private boolean isPassportIDValid() {
+        if (passportID.length() != 9) {
+            return false;
+        }
+        for (char c : passportID.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid() {
+        if (!allFieldsPresent()) {
+            return false;
+        }
+        if (!isBirthYearValid()) {
+            return false;
+        }
+        if (!isIssueYearValid()) {
+            return false;
+        }
+        if (!isExpirationYearValid()) {
+            return false;
+        }
+        if (!isHeightValid()) {
+            return false;
+        }
+        if (!isHairColorValid()) {
+            return false;
+        }
+        if (!isEyeColorValid()) {
+            return false;
+        }
+        if (!isPassportIDValid()) {
+            return false;
+        }
+        return true;
+    }
 }
